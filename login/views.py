ae4866a3609995be81601925cfa615861db4597d
mi8150jo <joetakutoo@gmail.com>
2024-02-22 05:40:41 +0900

    ログイン画面の実装

--- login/views.py
+++ login/views.py
@@ -5,4 +5,4 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def top(request):
     """トップ画面"""
-    return render(request,'allauth/layouts/bootstrap.html')
+    return render(request,'account/login_page.html')
